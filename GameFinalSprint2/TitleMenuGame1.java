228277deef3f05a534ff7330f35833855f8b8a31
M138A <0896375@hr.nl>
2014-12-09 13:23:00 +0100

    BackButton

--- GameFinalSprint2/TitleMenuGame1.java
+++ GameFinalSprint2/TitleMenuGame1.java
@@ -26,5 +26,7 @@ public class TitleMenuGame1 extends World
     addObject(instruction, 447 ,447);
     StartButtonGame1 button = new StartButtonGame1();
     addObject(button,450,750);
+    BackButton buttonback = new BackButton();
+    addObject(buttonback, 65,65);
     }
 }
