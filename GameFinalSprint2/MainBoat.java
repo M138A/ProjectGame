93a9f5abfe799ce53014fa711287ea208a597b08
barthr <bartfokker@gmail.com>
2014-12-12 11:40:55 +0100

    bootjes varen naar exit en je kan ze bewegen

--- GameFinalSprint2/MainBoat.java
+++ GameFinalSprint2/MainBoat.java
@@ -14,82 +14,90 @@ public class MainBoat extends Actor
     int slow  = 1; 
     int spawnCounter = 0;  
     int Score = 0;
-/** 
- *  Function counts till 5, 7 or 9 depending on the Boat when hitting the Dock1.class and than executes the MoveToExit
-**/
-public void Initiate() {
-    movement(); 
-    MouseMovementLeftRight();
-    StopBoat();
-    Colission();
-} 
+    boolean leeg = false;
+    /** 
+     *  Function counts till 5, 7 or 9 depending on the Boat when hitting the Dock1.class and than executes the MoveToExit
+     **/
+    public void Initiate() {
+        movement(); 
+        MouseMovementLeftRight();
+        StopBoat();
+        Colission();
+    } 
 
-public void ExitHarbor(int k)    
- {  
-      if (isTouching(Dock1.class)){
-          if (spawnCounter > k) {  
-          spawnCounter = 0;
-          MoveToExit();
-        }
-        spawnCounter++;
+    public void ExitHarbor(int k)    
+    {  
+        if (isTouching(Dock1.class)){
+            if (spawnCounter > k) {  
+                spawnCounter = 0;
+                MoveToExit();
+                leeg = true;
+            }
+            spawnCounter++;
         }
 
     }     
-    
-public void MoveToExit() {
-      slow = 1;
-      setLocation(getX(), getY() - 100);
-      setRotation(180);    
-}
-        
-/**  
- *  Function removes the object which crashes into another object from the class BoatBig
-*/
+
+    public void MoveToExit() {
+        slow = 1;
+        setLocation(getX(), getY() - 100);
+        setRotation(180);    
+    }
+
+    /**  
+     *  Function removes the object which crashes into another object from the class BoatBig
+     */
     public void Colission() {
         Actor haven = getOneIntersectingObject(Havenfoot2.class);
         Actor boat = getOneIntersectingObject(MainBoat.class);
-       if (haven != null || boat != null) {
+        if (haven != null || boat != null) {
             World world;
             world = getWorld();
             world.removeObject(this);
-            
-                     
-            } 
-        }
-/**
- * Function which makes the movement happen and also rotates at the given X and Y
-*/
 
-public void movement() {
-    move(-slow);
-     if (getX() <=  300 && getY() == 61) {
-       for(int i = 0; i <= 90; i++) {
-          //Greenfoot.delay(1);
-            setRotation(-i);
-            setLocation(getX() - slow, getY());
-        }
-    
-}
-}
-/**
- * Function to drag the boat from left to right to the dock
-*/
-public void MouseMovementLeftRight() {
-  if (Greenfoot.mouseDragged(this) == true){
-        MouseInfo mouse = Greenfoot.getMouseInfo();
-        setLocation(mouse.getX(), getY());
+        } 
     }
-}
 
-/**
- * Function which set var slow to 0 so the boat will between a certain X and Y
-*/
-public void StopBoat() {
+    /**
+     * Function which makes the movement happen and also rotates at the given X and Y
+     */
 
-    Actor boats = getOneIntersectingObject(ColissionDock.class);
+    public void movement() {
+        move(-slow);
+        if (getX() <=  300 && getY() == 61) {
+            for(int i = 0; i <= 90; i++) {
+                //Greenfoot.delay(1);
+                setRotation(-i);
+                setLocation(getX() - slow, getY());
+            }
+
+        }
+    }
+
+    /**
+     * Function to drag the boat from left to right to the dock
+     */
+    public void MouseMovementLeftRight() {
+        if (Greenfoot.mouseDragged(this) == true){
+            MouseInfo mouse = Greenfoot.getMouseInfo();
+            if (leeg){
+                setLocation(getX(), mouse.getY());
+            }
+            else {
+                setLocation(mouse.getX(),getY());
+            }
+        }
+    }
+
+    /**
+     * Function which set var slow to 0 so the boat will between a certain X and Y
+     */
+    public void StopBoat() {
+
+        Actor boats = getOneIntersectingObject(ColissionDock.class);
         if (boats != null){
             slow = 0;
         }
-    
-}
+
+    }
 }
