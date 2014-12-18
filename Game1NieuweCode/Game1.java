75628867e4a4537b41026676bd79fd92e47dc4ae
eldin61 <eldin.331@gmail.com>
2014-12-18 13:06:27 +0100

    spawn rate

--- Game1NieuweCode/Game1.java
+++ Game1NieuweCode/Game1.java
@@ -9,17 +9,94 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Game1 extends World
 {
     private Score score;
-    
+
     public Score getScore() {
         return score;
     }
-    
+
     public Game1()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(900, 900, 1); 
         prepare();
     }
+    private int spawnCounter = 0;  
+    private int spawnRateCounter = 0;  
+    public void act() {     
+        spawnRateCounter++;
+        if( spawnRateCounter <= 700)
+        {
+            spawnRateSlow();
+        } else if (spawnRateCounter >= 700 && spawnRateCounter <= 1400)
+        {
+            spawnRateMedium();
+        } else 
+        {
+            spawnRateFast();
+        }
+
+    }
+
+    public void spawnRateSlow()
+    {
+        if (spawnCounter > 500) {  
+            spawnCounter = 0;
+            double spawnRandom = Math.random();
+            if (spawnRandom <= 0.33)
+            {
+                addObject(new Boat(30,1,700,exit1), 800, 61);
+            } else if (spawnRandom >= 0.33 && spawnRandom <= 0.66)
+            {
+                addObject(new Boat(20,2,500,exit2), 800, 61);
+            } else 
+            {
+                addObject(new Boat(10,3,300,exit3), 800, 61);
+            }
+            spawnRandom = 0;
+        }  
+        spawnCounter++;  
+    }
+
+    public void spawnRateMedium()
+    {
+        if (spawnCounter > 300) {  
+            spawnCounter = 0;
+            double spawnRandom = Math.random();
+            if (spawnRandom <= 0.33)
+            {
+                addObject(new Boat(30,1,700,exit1),800,61);
+            } else if (spawnRandom >= 0.33 && spawnRandom <= 0.66)
+            {
+                addObject(new Boat(20,2,500,exit2), 800, 61);
+            } else 
+            {
+                addObject(new Boat(10,3,300,exit3), 800, 61);
+            }
+            spawnRandom = 0;
+        }  
+        spawnCounter++;  
+    }
+
+    public void spawnRateFast()
+    {
+        if (spawnCounter > 200) {  
+            spawnCounter = 0;
+            double spawnRandom = Math.random();
+            if (spawnRandom <= 0.33)
+            {
+                addObject(new BoatBig(),800,61);
+            } else if (spawnRandom >= 0.33 && spawnRandom <= 0.66)
+            {
+                addObject(new Boat(20,2,500,exit2), 800, 61);
+            } else 
+            {
+                addObject(new Boat(10,3,300,exit3), 800, 61);
+            }
+            spawnRandom = 0;
+        }  
+        spawnCounter++;  
+    }
+
     public void prepare()
     {
         Exit exit1 = new Exit("paars");
